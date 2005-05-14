5187664d401c98bd30a565c2f65e4e0e229e8607
mattbehrens <>
2005-05-14 01:42:15 +0000

    initial import

---
+++ dbbsave.py
@@ -0,0 +1,104 @@
+# $Id$
+#
+# Copyright (c) 2005 Matt Behrens.
+#
+# Permission is hereby granted, free of charge, to any person obtaining
+# a copy of this software and associated documentation files (the
+# "Software"), to deal in the Software without restriction, including
+# without limitation the rights to use, copy, modify, merge, publish,
+# distribute, sublicense, and/or sell copies of the Software, and to
+# permit persons to whom the Software is furnished to do so, subject to
+# the following conditions:
+#
+# The above copyright notice and this permission notice shall be included
+# in all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY
+# KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE
+# WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+# NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+# LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+# OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+# WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+"""
+Daigasso! Band Brothers DS save manipulation
+"""
+
+import nincodec
+
+GAME_DATA_SIZE = 0x8000
+SCORE_SIZE = 0x6000
+PADDING_SIZE = 0x8000
+
+class GameData(object):
+    """
+    Daigasso! Band Brothers game data
+    """
+
+    def __init__(self, file):
+        self.data = file.read(GAME_DATA_SIZE)
+        if len(self.data) != GAME_DATA_SIZE:
+            raise IOError, "short read"
+        
+        if self.data[:8] != "GBMDGSBB":
+            raise ValueError, "game data magic mismatch"
+
+    def write(self, file):
+        file.write(self.data)
+
+class Score(object):
+    """
+    Daigasso! Band Brothers score
+    """
+
+    def __init__(self, file=None, raise_on_invalid=True):
+        if file is None:
+            self.data = "\x00" * SCORE_SIZE
+            self.valid = False
+            self.title = None
+
+        else:
+            self.data = file.read(SCORE_SIZE)
+            if len(self.data) != SCORE_SIZE:
+                raise IOError, "short read"
+
+            if self.data[:8] == "BBRS_GAK":
+                self.valid = True
+                self.title = \
+                        self.data[0x40:0x60].decode("nintendo").strip("\x00")
+            else:
+                if raise_on_invalid:
+                    raise ValueError, "score magic mismatch"
+                else:
+                    self.valid = False
+                    self.title = None
+
+    def write(self, file):
+        file.write(self.data)
+
+class Save(object):
+    """
+    Daigasso! Band Brothers save
+    """
+
+    def __init__(self, file):
+        self.game_data = GameData(file)
+
+        self.scores = []
+        for n in range(8):
+            self.scores.append(Score(file, raise_on_invalid=False))
+
+        self._padding = file.read(PADDING_SIZE)
+        if len(self._padding) != PADDING_SIZE:
+            raise IOError, "short read"
+
+    def write(self, file):
+        self.game_data.write(file)
+
+        for n in range(8):
+            self.scores[n].write(file)
+
+        file.write(self._padding)
+
+# ex:et:sw=4:ts=4
---
+++ dsmax.py
@@ -0,0 +1,95 @@
+# $Id$
+#
+# Copyright (c) 2005 Matt Behrens.
+#
+# Permission is hereby granted, free of charge, to any person obtaining
+# a copy of this software and associated documentation files (the
+# "Software"), to deal in the Software without restriction, including
+# without limitation the rights to use, copy, modify, merge, publish,
+# distribute, sublicense, and/or sell copies of the Software, and to
+# permit persons to whom the Software is furnished to do so, subject to
+# the following conditions:
+#
+# The above copyright notice and this permission notice shall be included
+# in all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY
+# KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE
+# WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+# NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+# LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+# OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+# WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+"""
+Support for Datel's Action Replay DS MAX image files
+"""
+
+import codecs
+
+def nullpad(s, width):
+    """
+    Pad a string with NULs to a given width
+    """
+
+    s = s + "\x00" * (width - len(s))
+    return s[:width]
+
+class SaveFile(object):
+    """
+    Action Replay DS MAX save image file (.dss)
+    """
+
+    def __init__(self, file, save_class):
+        self._magic = file.read(16)
+
+        if self._magic != "ARDS000000000001":
+            raise ValueError, "file magic mismatch"
+
+        # These look like NUL-padded UTF-16; cross your fingers
+        self.game_name = (codecs.BOM_UTF16_LE +
+                file.read(64)).decode("utf-16")
+        self.save_name = (codecs.BOM_UTF16_LE +
+                file.read(64)).decode("utf-16")
+        self.description = (codecs.BOM_UTF16_LE +
+                file.read(126)).decode("utf-16")
+
+        # Remainder of header is currently opaque to us; there appear to be
+        # ASCII copies of the text data later but it is unused by the
+        # user interface and unpredictable
+        self._opaque = file.read(230)
+
+        if len(self._magic) != 16 or \
+                len(self.game_name) != 32 or \
+                len(self.save_name) != 32 or \
+                len(self.description) != 63 or \
+                len(self._opaque) != 230:
+            raise IOError, "short read"
+
+        self.game_name = self.game_name.rstrip("\x00")
+        self.save_name = self.save_name.rstrip("\x00")
+        self.description = self.description.rstrip("\x00")
+
+        # Remainder of file is the actual save
+        self.save = save_class(file)
+
+    def write(self, file):
+        """
+        Write the save image to a file
+        """
+
+        file.write(self._magic)
+
+        # Write UTF-16-encoded strings with the byte order marker removed
+        file.write(nullpad(self.game_name,
+            32).encode("utf-16")[len(codecs.BOM_UTF16_LE):])
+        file.write(nullpad(self.save_name,
+            32).encode("utf-16")[len(codecs.BOM_UTF16_LE):])
+        file.write(nullpad(self.description,
+            63).encode("utf-16")[len(codecs.BOM_UTF16_LE):])
+
+        file.write(self._opaque)
+
+        self.save.write(file)
+
+# ex:et:sw=4:ts=4
